61c06f21577976f5070d47c7fd4d42d33c276f07
asuhaib2002 <asuhaib2002@gmail.com>
2024-07-10 16:27:48 +0500

    Initial commit

---
+++ eventico/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from eventico.users.models import User
+from eventico.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def _media_storage(settings, tmpdir) -> None:
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture()
+def user(db) -> User:
+    return UserFactory()
