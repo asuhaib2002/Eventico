e2cb2c3670143f5c39ddee6c3c3c3236f5b875e0
asuhaib2002 <asuhaib2002@gmail.com>
2024-07-11 17:26:42 +0500

    feat(event): restricted put, patch, delete to object owner only

--- eventico/event_manager/api/views.py
+++ eventico/event_manager/api/views.py
@@ -1,8 +1,11 @@
 from rest_framework import viewsets
-from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly, BasePermission
 from event_manager.models import Event
 from .serializers import EventSerializer
 
+class IsOwner(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return obj.owner == request.user
 class EventViewSet(viewsets.ModelViewSet):
     queryset = Event.objects.all()
     serializer_class = EventSerializer
@@ -10,6 +13,8 @@ class EventViewSet(viewsets.ModelViewSet):
     def get_permissions(self):
         if self.request.method in ['GET']:
             self.permission_classes = [IsAuthenticatedOrReadOnly]
+        elif self.request.method in ['PUT', 'PATCH', 'DELETE']:
+            self.permission_classes = [IsOwner]
         else:
             self.permission_classes = [IsAuthenticated]
         return super().get_permissions()
